be7610d75a0de86bf70e1c23ab5498be6f0cc934
Ni He <ni.he@qq.com>
2020-04-14 01:04:19 +0800

    update

--- data_process.py
+++ data_process.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Apr  4 23:50:16 2020
-
 @author: NI HE
 """
 
@@ -13,6 +12,9 @@ from datetime import datetime
 from iexfinance.stocks import get_historical_data
 import matplotlib.pyplot as plt
 from scipy import stats
+
+tushare_token = '1c8b06446534ae510c8c68e38fc248b99f89ac3814cb55645ae2be72'
+pro = ts.pro_api(tushare_token)
 
 def collect_stock_data(code, start_date, end_date, is_chinese = 1):
     my_token = 'pk_20fe28167fc34f56adba1d341120cb0e' 
@@ -38,6 +40,13 @@ def collect_stock_data(code, start_date, end_date, is_chinese = 1):
         
     return stk_data
 
+def collect_stock_data_pro(code, start_date, end_date):
+    try:
+        stk_data = pro.daily(ts_code = code, start_date = start_date, end_date = end_date)
+    except:
+        print('Stock '+code+' could not collect from Tushare Pro')
+    return stk_data
+
 def tick2ret(price):
     return 100 * (price - price.shift(1)).div(price.shift(1))[1:]
 
@@ -45,12 +54,27 @@ def tick2ret(price):
 def random_pick_stocks_code(n):
     industry = ts.get_industry_classified()
     ind = [x[0] for x in list(np.random.randint(low = 0, high = len(industry), size = (n, 1)))]
-    return industry.iloc[ind]['code']
+    return industry.iloc[ind]['code'] 
+
+def random_pick_stocks_code_pro(n): 
+    stock_list = pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
+    ind = [x[0] for x in list(np.random.randint(low = 0, high = len(stock_list), size = (n, 1)))]
+    return stock_list.iloc[ind]['ts_code'] 
 
 def random_stocks_return(start_date, end_date, n):
     ret_list = pd.DataFrame()
     for c in random_pick_stocks_code(n):
         st = collect_stock_data(c,start_date,end_date)    
+        st_ret = tick2ret(st['close']).to_frame(name = c) 
+        ret_list = pd.concat([ret_list, st_ret], axis = 1 ,join = 'outer', ignore_index = False, sort = False)
+    ret_list.dropna(axis = 0, how = 'any', inplace = True)
+    return ret_list
+
+def random_stocks_return_pro(start_date, end_date, n):
+    ret_list = pd.DataFrame()
+    for c in [i for i in random_pick_stocks_code_pro(n)]:
+        st = collect_stock_data_pro(c,start_date,end_date)
+        st.set_index(["trade_date"], inplace=True)
         st_ret = tick2ret(st['close']).to_frame(name = c) 
         ret_list = pd.concat([ret_list, st_ret], axis = 1 ,join = 'outer', ignore_index = False, sort = False)
     ret_list.dropna(axis = 0, how = 'any', inplace = True)
--- portfolio_ini.py
+++ portfolio_ini.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Apr  5 02:15:33 2020
-
 @author: Ni He
 """
 
@@ -9,23 +8,46 @@ import data_process
 import numpy as np
 import matplotlib.pyplot as plt
 
+def get_stock_data(start_date, end_date, num_assets):
+    return data_process.random_stocks_return_pro(start_date, end_date, num_assets)
+    
+
+def port_stat(ret_list, num_assets, num_ports):
+
+    weights = np.random.random([num_ports,num_assets])
+    weights = weights / (np.mat(np.sum(weights, axis = 1)).T* np.ones([1,num_assets]))
+    cov_matrix = np.cov(ret_list.T)
+    
+    risk_matrix = weights * cov_matrix * weights.T
+    ports_risks = np.sqrt(np.diag(risk_matrix))
+    ports_returns = np.array(weights * np.mat(np.mean(ret_list.values, axis = 0)).T)
+    
+    return ports_risks, ports_returns, cov_matrix
+    
+    
+
 
 start_date='2017-07-01'
 end_date='2020-3-30'
-n = 5
-ret_list = data_process.random_stocks_return(start_date, end_date, n)
+num_assets = 3
+ret_list = get_stock_data(start_date, end_date, num_assets)
 
 
-# calculate portfolio risk based on covariance matrix
+# calculate portfolio with 2 assets
+num_ports = 100
+n = 2
+ports_risks, ports_returns, cov_matrix = port_stat(ret_list.iloc[:,:n], n, num_ports)
+
+plt.figure()
+plt.scatter(ports_risks, ports_returns, alpha=0.4)
+plt.scatter(np.sqrt(np.diag(cov_matrix)), np.mean(ret_list.iloc[:,:n].values, axis = 0), c = '#FF0000', alpha=0.4)
+
+
+# calculate portfolio with more than 2 assets
+
 num_ports = 500
-num_assets = 5
-weights = np.random.random([num_ports,num_assets])
-weights = weights / (np.mat(np.sum(weights, axis = 1)).T* np.ones([1,num_assets]))
-
-cov_matrix = np.cov(ret_list.T)
-
-risk_matrix = weights * cov_matrix * weights.T
-ports_risks = np.sqrt(np.diag(risk_matrix))
+n = 3
+ports_risks, ports_returns, cov_matrix = port_stat(ret_list.iloc[:,:n], n, num_ports)
 
 plt.figure()
 plt.hist(ports_risks, bins = 20)
@@ -34,6 +56,18 @@ plt.show()
 print('The average risks of components stocks is: ' + str(np.mean(np.sqrt(np.diag(cov_matrix)))))
 print('The average risks of portfolios is: ' + str(np.mean(ports_risks)))
 
-ports_returns = np.array(weights * np.mat(np.mean(ret_list.values, axis = 0)).T)
 plt.figure()
-plt.scatter(ports_risks, ports_returns)
+plt.scatter(ports_risks, ports_returns, alpha=0.4)
+plt.scatter(np.sqrt(np.diag(cov_matrix)), np.mean(ret_list.iloc[:,:n].values, axis = 0), c = '#FF0000', alpha=0.4 )
+
+
+## potential of diversification 
+#num_ports = 100
+#min_risks = []
+#for n in range(2,10):
+#    ports_risks, ports_returns, cov_matrix = port_stat(ret_list.iloc[:,:n], n, num_ports)
+#    min_risks.append(np.min(ports_risks))
+#plt.figure()
+#plt.plot(min_risks)
+#plt.scatter(range(len(min_risks)), min_risks, alpha = 0.9)
+    
